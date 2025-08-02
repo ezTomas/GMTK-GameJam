73f805a5eb539859b541294734c407391a032945
Enzo Tomas <195832517+ezTomas@users.noreply.github.com>
2025-08-02 12:21:18 -0300

    Nivel 4

---
+++ gmtk-gamejam/Escenas/satelite.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+@export var radius: float = 650.0  
+@export var speed: float = 1.0   
+
+var angle: float = 0.0
+var planet: Node2D
+
+func _ready():
+	planet = get_node("/root/Mundo4/Planet")
+
+func _process(delta):
+
+	if planet:
+		angle += speed * delta
+		var planet_pos = planet.global_position
+		position.x = planet_pos.x + cos(angle) * radius
+		position.y = planet_pos.y + sin(angle) * radius
+		
+		
+		rotation = (planet_pos - position).angle()
