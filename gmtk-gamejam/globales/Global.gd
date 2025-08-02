c7b0174fc787f2b6f5157697ef43b08ed1b6f901
Enzo Tomas <195832517+ezTomas@users.noreply.github.com>
2025-08-02 12:44:31 -0300

    Correcion

--- gmtk-gamejam/globales/Global.gd
+++ gmtk-gamejam/globales/Global.gd
@@ -1,11 +1,11 @@
 extends Node
 
+var niveles = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var monedas_nivel_1 = 0
 
+var monedas_nivel_2 = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var monedas_nivel_3 = 0
+
+var monedas_nivel_4 = 0
